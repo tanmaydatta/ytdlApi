e4e339a78639dac9e3e254d62e75448fe45d210f
Tanmay Datta <tanmay.datta@inmobi.com>
2017-11-20 16:14:39 +0530

    get video url

--- ytdlAPI/controller/__init__.py
+++ ytdlAPI/controller/__init__.py
@@ -1,2 +1,18 @@
+import youtube_dl
+
+ydl_opts = {
+         'format': 'best',
+         'postprocessors': [{
+             'key': 'FFmpegExtractAudio',
+             'preferredcodec': 'mp3',
+             'preferredquality': '192',
+        }],
+    }
+
 def index():
     return '<h1>It works!</h1>'
+
+def get_video_url(video_id):
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        res = ydl.extract_info("https://www.youtube.com/watch?v=" + video_id, download=False)
+    return res["url"]
--- ytdlAPI/router.py
+++ ytdlAPI/router.py
@@ -1,5 +1,6 @@
 from ytdlAPI import app
-from ytdlAPI.controller import index
+from ytdlAPI.controller import index, get_video_url
 
 
 app.add_url_rule('/', 'index', index)
+app.add_url_rule('/<video_id>/', 'video', get_video_url)
